ea211c332f8c9a9a52521b7877b7c40d6c856082
bywwh <1065449148@qq.com>
2016-01-01 17:00:30 +0800

    added FLAC support

--- device.mk
+++ device.mk
@@ -95,7 +95,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/rootdir/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/rootdir/etc/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/rootdir/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/rootdir/etc/media_codecs_xiaomi_extra.xml:system/etc/media_codecs_xiaomi_extra.xml
 
 # BT
 PRODUCT_COPY_FILES += \
